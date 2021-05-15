b2c73a92b83936685b72a8ef15a412acbbb1638a
Thomasjnliu <thomasjnliu@gmail.com>
2021-05-15 00:50:04 -0400

    Add player sprites

---
+++ Assets/Scripts/SineWaveHover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//this only works for sprite objects
+public class SineWaveHover : MonoBehaviour
+{
+    [SerializeField] float effectSpeed = 1;
+    [SerializeField] float effectMultiplier = 1;
+
+    float yPos;
+    Vector3 originalPos;
+
+    private void Start()
+    {
+        originalPos = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yPos = Mathf.Sin(Time.time * effectSpeed) * effectMultiplier;
+        transform.localPosition = originalPos + new Vector3(0, yPos, 0);
+    }
+}
